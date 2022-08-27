b08e00629ca2abf7226881f8fc6e13c7e21025f0
relation <91399393+relationlabs@users.noreply.github.com>
2022-08-27 23:03:14 +0800

    Add files via upload

---
+++ src/runtime/src/wasm_binary.rs
@@ -0,0 +1,2 @@
+pub const WASM_BINARY: Option<&[u8]> = Some(include_bytes!("../wasm/node_template_runtime.compact.compressed.wasm"));
+pub const WASM_BINARY_BLOATY: Option<&[u8]> = Some(include_bytes!("../wasm/node_template_runtime.wasm"));
